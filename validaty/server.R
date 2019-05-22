eb09ae698d4f857bf6d816c2483095d62414b4c3
Bernhard Meindl <bernhard.meindl@gmail.com>
2019-05-22 15:09:07 +0200

    prelim work to add/remove and edit rules

--- validaty/server.R
+++ validaty/server.R
@@ -1,23 +1,19 @@
-#
-# This is the server logic of a Shiny web application. You can run the 
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 library(errorlocate)
 library(validatetools)
 library(data.table)
 library(validate)
-
+library(shinyjs)
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output, session) {
   
-  ## reading data file ----
+  RulesAvailable <- reactiveVal(FALSE)
+  DataAvailable <- reactiveVal(FALSE)
+  RulesDF <- reactiveVal(NULL) # rules as a data.frame
+  RuleSet <- reactiveVal(NULL) # set of rules
+  
+  # reading data file ----
   DataFile <- reactive({
     # If no file is selected, don't do anything
     validate(need(input$datafile, message = FALSE))
@@ -26,66 +22,173 @@ shinyServer(function(input, output, session) {
   
   # The user's data, parsed into a data frame
   DataSet <- reactive({
-    fread(input=DataFile()$datapath)
+    DataAvailable(TRUE)
+    ff <- fread(input = DataFile()$datapath)
+    ff
   })
   
-  output$datatable <- renderDataTable({
-    DataSet()
+  output$datatable <- shiny::renderDataTable({
+   DataSet()
   })
   
   observe({
-    data <- DataSet()
-    updateSelectInput(session, inputId = "key", choices = c("no key", names(DataSet())))
+   data <- DataSet()
+   updateSelectInput(session, inputId = "key", choices = c("no key", names(DataSet())))
   })
-  
-  
+
+
   ## Reading rule file ----
-  
   RuleFile <- reactive({
-    # If no file is selected, don't do anything
-    validate(need(input$rulefile, message = FALSE))
-    input$rulefile
+   # If no file is selected, don't do anything
+   validate(need(input$rulefile, message = FALSE))
+   input$rulefile
   })
   
   # The user's data, parsed into a data frame
-  RuleSet <- reactive({
-    validate::validator(.file=RuleFile()$datapath)
-  })
-  
-  output$rules <- renderDataTable({
-     as.data.frame(RuleSet())[c("name","label","rule")]
-  })
-  
   observe({
-    df <- as.data.frame(RuleSet())
-    updateSelectizeInput(session, inputId = "modifyrulename", choices = df$name, server = TRUE)
+    rr <- validate::validator(.file = RuleFile()$datapath)
+    RuleSet(rr)
+    RulesDF(as.data.frame(rr))
+  })
+  
+  ## show modify rules ui
+  observe({
+    if (RulesAvailable() & DataAvailable()) {
+      shinyjs::show("modify_content")
+      shinyjs::hide("modify_placeholder")
+      
+    } else {
+      shinyjs::hide("modify_content")
+      shinyjs::show("modify_placeholder")
+    }
+  })
+  
+  output$rules <- shiny::renderDataTable({
+    RulesDF()[, c("name", "label", "rule")]
+  })
+
+  observe({
+    df <- RulesDF()
+    nn <- df$name
+    if (input$modify_what != "add") {
+      shiny::updateSelectInput(
+        session = session, 
+        inputId = "modify_rulename", 
+        choices = nn)
+    }
+    RulesAvailable(TRUE)
   })  
   
-  observeEvent(input$modifyrulename, {
-    #rule <- input$modify_rulename
-    #print(str(rule))
-    message("firing!")
+  observeEvent(input$modify_what, {
+    sel <- input$modify_what
+    if (sel == "add") {
+      shinyjs::hide(id = "modify_rulename")
+      shiny::updateTextInput(
+        session = session,
+        inputId = "modify_editrule",
+        value = "")
+      btn_txt <- "Add the rule"
+    } else {
+      shinyjs::show(id = "modify_rulename")
+      if (sel == "remove") {
+        btn_txt <- "Remove the selected rule"
+      } else {
+        btn_txt <- "Edit the selected rule"
+      }
+    }
     
-    #if (rule != "") {
-    #  message("if")
-    #  df <- as.data.frame(RuleSet())
-    #  expr <- df[df$name == input$modify_rulename, "rule"]
-    #  updateTextInput(session, inputId = "modify_editrule", value = expr)
-    #}
-  })  
+    if (sel == "remove") {
+      shinyjs::hide(id = "modify_editrule")
+    } else {
+      shinyjs::show(id = "modify_editrule")
+    }
+    
+    # updating btn label
+    shiny::updateActionButton(
+      session = session,
+      inputId = "btn_rule_modify",
+      label = btn_txt)
+    
+  }, ignoreInit = TRUE)
   
+  observeEvent(input$modify_rulename, {
+    rule <- input$modify_rulename
+    df <- RulesDF()
+    expr <- df[df$name == input$modify_rulename, "rule"]
+    updateTextInput(session, inputId = "modify_editrule", value = expr)
+  }, ignoreInit = TRUE, priority = 10)  
+
+  # delete a rule
+  observeEvent(input$btn_rule_modify, {
+    tmpf <- tempfile()
+    what <- input$modify_what
+    if (what == "add") {
+      # write current rule to file
+      cat(input$modify_editrule, "\n", file = tmpf)
+      dat <- DataSet()
+      res <- confront(dat = dat[1, ], validator(.file = tmpf))
+      
+      if (length(errors(res)) == 0) {
+        # read all rules, add the new one, write to file and update everything
+        rr <- as.character(RulesDF()$rule)
+        rr <- c(rr, input$modify_editrule)
+        cat(rr, "\n", sep = "\n", file = tmpf)
+        val <- validator(.file = tmpf)
+        df <- as.data.frame(val)
+      } else {
+        message("your rule was not valid --> Todo: show warning")
+        shiny::updateTextInput(
+          session = session, inputId = "modify_editrule", value = ""
+        )
+      }
+    } else if (what == "edit") {
+      message("editing a rule")
+      
+      # check current new rule
+      cat(input$modify_editrule, "\n", file = tmpf)
+      dat <- DataSet()
+      res <- confront(dat = dat[1, ], validator(.file = tmpf))
+      
+      if (length(errors(res)) == 0) {
+        rr <- RulesDF()
+        ii <- rr$name == input$modify_rulename
+        rr <- as.character(rr$rule)
+        rr[ii] <- input$modify_editrule
+        cat(rr, "\n", sep = "\n", file = tmpf)
+        val <- validator(.file = tmpf)
+        df <- as.data.frame(val)
+      } else {
+        df <- RulesDF()
+        val <- RuleSet()
+      }
+    } else if (what == "remove") {
+      rr <- RulesDF()
+      rr <- rr[rr$name != input$modify_rulename, ]
+      rr <- as.character(rr$rule)
+      if (length(rr) > 0) {
+        cat(rr, "\n", sep = "\n", file = tmpf)
+        val <- validator(.file = tmpf)
+        df <- as.data.frame(val)
+      } else {
+        # no rules left
+        val <- df <- NULL
+      }
+    }
+    # updating inputs    
+    RuleSet(val)
+    RulesDF(df)
+    
+    if (file.exists(tmpf)) {
+      file.remove(tmpf)
+    }
+    # switching tabs
+    updateTabsetPanel(
+      session = session, 
+      inputId = "ts_rules",
+      selected = "tp_view"
+    )   
+  })
   
-  
-  
-  # observe({
-  #   updateSelectizeInput(session, "selected_rule"
-  #             , choices=c("Add new",names(RuleSet())),
-  #             server = TRUE)
-  # })
-  # observe({
-  #   print(input$selected_rule)
-  # })
-   
   
   # Download rules as yaml file
   output$my_rules <- downloadHandler(
@@ -96,60 +199,89 @@ shinyServer(function(input, output, session) {
   )
 
   ## Variable coverage ----
-  
   output$ruleplot <- renderPlot({
     plot(RuleSet())
   })
-  
+
   output$variablesCovered <- renderText({
-    vrs <- paste(variables(RuleSet()),collapse=", ")
-    HTML("<b>Variables covered: </b>", vrs)  
-    })
-  
+    vrs <- paste(variables(RuleSet()), collapse = ", ")
+    HTML("<b>Variables covered: </b>", vrs)
+  })
+
   output$variablesNotCovered <- renderText({
-    validate(need(DataSet(),"No data loaded"))
+    validate(need(DataSet(), "No data loaded"))
     vrs <- names(DataSet())
     vrs <- vrs[!vrs %in% variables(RuleSet())]
-    HTML("<b>Variables not covered: </b> ",paste(vrs, collapse=", "))
+    HTML("<b>Variables not covered: </b> ", paste(vrs, collapse = ", "))
   })
 
   ## Rule inspection ----
   observe({
     rls <- RuleSet()
-    fixed <- validatetools::detect_fixed_variables(rls)
-    fixed <- if (is.null(fixed)){
-      data.frame(variable=character(0), value=numeric(0))
-    } else {
-      data.frame(variable=names(fixed), value=sapply(fixed, identity))
+    if (!is.null(rls)) {
+      fixed <- tryCatch(
+        validatetools::detect_fixed_variables(rls),
+        error = function(e) e
+      )
+      if (!inherits(fixed, "error")) {
+        fixed <- if (is.null(fixed)) {
+          data.frame(
+            variable = character(0), 
+            value = numeric(0)
+          )
+        } else {
+          data.frame(
+            variable = names(fixed),
+            value = sapply(fixed, identity))
+        }
+        
+        num_bdr <- validatetools::detect_boundary_num(rls)
+        cat_bdr <- validatetools::detect_boundary_cat(rls)
+        
+        output$cat_bdr <- shiny::renderDataTable(cat_bdr)
+        output$num_bdr <- shiny::renderDataTable(num_bdr)
+        output$fixed_variables <- shiny::renderDataTable(fixed)
+        
+      }
     }
-    
-    num_bdr <- validatetools::detect_boundary_num(rls)
-    cat_bdr <- validatetools::detect_boundary_cat(rls)
-    
-    output$cat_bdr <- renderDataTable(cat_bdr)
-    output$num_bdr <- renderDataTable(num_bdr)
-    output$fixed_variables <- renderDataTable(fixed)
   })
-  
-  ## Confrontation ----
+  # 
+  # ## Confrontation ----
   observe({
-    updateNumericInput(session
-            , inputId = "lin.eq.eps"
-            , value = voptions(RuleSet(), "lin.eq.eps"))
+    shiny::updateNumericInput(
+      session = session,
+      inputId = "lin.eq.eps",
+      value = voptions(RuleSet(), "lin.eq.eps")
+    )
   })
   
-  ResultSet <- reactive({
-    confront(DataSet()
-       , RuleSet()
-       , key = ifelse(input$key == "no key", NA_character_, input$key)
-       , lin.eq.eps=input$lin.eq.eps)
+  ResultSet <- reactiveVal(NULL)
+  
+  observeEvent(input$btn_confront, {
+    res <- validate::confront(
+      dat = DataSet(),
+      x = RuleSet(),
+      key = ifelse(input$key == "no key", NA_character_, input$key),
+      lin.eq.eps = input$lin.eq.eps
+    )
+    ResultSet(res)
   })
-  
-  output$resultset <- renderDataTable(summary(ResultSet()))
-  output$confrontationplot <- renderPlot(plot(ResultSet(),main="Results by rule"))
-  
-  output$detailset <- renderDataTable(as.data.frame(ResultSet()))
-  
+  # 
+  # ResultSet <- reactive({
+  #   confront(
+  #     DataSet(),
+  #     RuleSet(),
+  #     key = ifelse(input$key == "no key", NA_character_, input$key),
+  #     lin.eq.eps = input$lin.eq.eps
+  #   )
+  # })
+  # 
+  output$resultset <- shiny::renderDataTable(summary(ResultSet()))
+  output$confrontationplot <-
+    renderPlot(plot(ResultSet(), main = "Results by rule"))
+
+  output$detailset <- shiny::renderDataTable(as.data.frame(ResultSet()))
+
   # Download details as csv
   output$detailed_results <- downloadHandler(
     filename = "detailed_results.csv"
@@ -158,48 +290,47 @@ shinyServer(function(input, output, session) {
       write.csv(as.data.frame(ResultSet()), file, row.names = FALSE)
     }
   )
-
-  # Download plot in a pdf file 
+  # Download plot in a pdf file
   output$report = downloadHandler(
     filename = "report.pdf",
-    
+
     content = function(file) {
       pdf(file) # open the pdf device
-      plot(ResultSet(),main="Results by rule")
+      plot(ResultSet(), main = "Results by rule")
       dev.off()
     }
   )
-  
-  
-  ## Error localization ----
-  error_locs <- reactive({
-    locate_errors(DataSet(), RuleSet())
-  })
-  el_summary <- reactive({
-    summary(error_locs())
-  })
-  el_rules <- reactive({
-    el_summary()[[1]][el_summary()[[1]]$errors > 0, "name"]
-  })
-  
-  output$el_var  <- renderDataTable(el_summary()[[1]])
-  output$el_rec  <- renderDataTable(as.data.table(el_summary()[[2]]))
-  
-  observe({
-    erronous_rules <- el_rules()
-    updateSelectInput(session, inputId = "rules", choices = erronous_rules)
-  })
-  
-  error_ind <- reactive({
-    which(values(error_locs()), arr.ind = TRUE)
-  })
-  
-  val_values <- reactive({
-    values(ResultSet())
-  })
-
-  output$el_rows <- renderDataTable(
-     subset(as.data.frame(error_locs()), apply(values(error_locs()),1,any))
-  )
+  # 
+  # 
+  # ## Error localization ----
+  # error_locs <- reactive({
+  #   locate_errors(DataSet(), RuleSet())
+  # })
+  # el_summary <- reactive({
+  #   summary(error_locs())
+  # })
+  # el_rules <- reactive({
+  #   el_summary()[[1]][el_summary()[[1]]$errors > 0, "name"]
+  # })
+  # 
+  # output$el_var  <- renderDataTable(el_summary()[[1]])
+  # output$el_rec  <-
+  #   renderDataTable(as.data.table(el_summary()[[2]]))
+  # 
+  # observe({
+  #   erronous_rules <- el_rules()
+  #   updateSelectInput(session, inputId = "rules", choices = erronous_rules)
+  # })
+  # 
+  # error_ind <- reactive({
+  #   which(values(error_locs()), arr.ind = TRUE)
+  # })
+  # 
+  # val_values <- reactive({
+  #   values(ResultSet())
+  # })
+  # 
+  # output$el_rows <- renderDataTable(subset(as.data.frame(error_locs()), apply(values(error_locs(
+  # )), 1, any)))
 }) 
   
--- validaty/ui.R
+++ validaty/ui.R
@@ -1,12 +1,3 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 library(data.table)
 library(validate)
@@ -14,114 +5,125 @@ library(validate)
 # Define UI for application that draws a histogram
 shinyUI(
   navbarPage(
+    useShinyjs(),
     title = "Validaty",
-    
     tabPanel("Data Input",
-             sidebarLayout(
-               sidebarPanel(
-                 shiny::fileInput("datafile","CSV file"),
-                 shiny::selectInput("key", "Select key variable",
-                                    "no key")),
-               mainPanel(
-                 dataTableOutput("datatable"))
-    )),
+      sidebarLayout(
+        sidebarPanel(
+          shiny::fileInput("datafile","CSV file"),
+          shiny::selectInput("key", "Select key variable", "no key")
+        ),
+        mainPanel(
+          shiny::dataTableOutput("datatable")
+        )
+      )
+    ),
     tabPanel("Rule Management",
-             sidebarLayout(
-               sidebarPanel(
-                 fileInput("rulefile","Free text/YAML")),
-               mainPanel(
-                 tabsetPanel(
-                   tabPanel("View Rules",
-                            shiny::dataTableOutput("rules"),
-                            # Download Rules Button
-                            downloadButton("my_rules", "Download Rules")
-                            ),
-                   
-                   tabPanel("Modify rules",
-                      h3("Modify "),
-                      shiny::radioButtons(inputId = "modify_what",
-                                          label = "What do you want to do?",
-                                          choices = c("add", "remove", "edit"),
-                                          inline = TRUE),
-                      shiny::selectizeInput(inputId = "modifyrulename",
-                                         label = "select a rule",
-                                         selected = NULL,
-                                         choices = c()),
-                      shiny::textInput(inputId = "modify_editrule",
-                                       label = "add/edit rule",
-                                       value = "")
-                      
-                      
-                   #     shiny::selectInput("selected_rule","Select rule"
-                   #                        , choices=c("Add new","B"),selected = "B"),
-                   #     shiny::textAreaInput("rule_exp","Expression"),
-                   #     shiny::textInput("rule_name","Name"),
-                   #     shiny::textInput("rule_label","Lalel"),
-                   #     shiny::textInput("rule_description","Description")
-                   #     # shiny::actionButton("rule_update","Update")
-                        ),
-                   tabPanel("Coverage",
-                            shiny::plotOutput("ruleplot"),
-                            shiny::htmlOutput("variablesCovered"),
-                            shiny::htmlOutput("variablesNotCovered")
-                            ),
-                   tabPanel("Imposed limits",
-                            tags$h3("Boundaries"),
-                            tags$em("Limits imposed by the rule set"),
-                            shiny::dataTableOutput("num_bdr"),
-                            shiny::dataTableOutput("cat_bdr"),
-                            tags$h3("Fixed values"),
-                            tags$em("Variables which can only take one value under the current rule set"),
-                            shiny::dataTableOutput("fixed_variables")
-                            )
-               )))
-                 ),
+      sidebarLayout(
+        sidebarPanel(
+          fileInput("rulefile","Free text/YAML")
+        ),
+        mainPanel(
+          tabsetPanel(id = "ts_rules",
+            tabPanel("View Rules", value = "tp_view",
+              shiny::dataTableOutput("rules"),
+              # Download Rules Button
+              downloadButton("my_rules", "Download Rules")
+            ),
+            tabPanel("Modify rules",
+            h3("Update, Add or Delete rules"),
+            
+            div(id = "modify_placeholder", 
+              h3("Please upload some rules first")
+            ),
+            div(id = "modify_content", 
+              shiny::radioButtons(
+                inputId = "modify_what",
+                label = "What do you want to do?",
+                choices = c(
+                  "Add a rule" = "add", 
+                  "Remove a rule" = "remove", 
+                  "Edit a rule" = "edit"),
+                inline = TRUE),
+              hidden(shiny::selectInput(
+                inputId = "modify_rulename",
+                label = "select a rule",
+                selected = NULL,
+                choices = c())),
+              shiny::textInput(
+                inputId = "modify_editrule",
+                label = "add/edit rule",
+                value = ""),
+              shiny::actionButton(inputId = "btn_rule_modify", label = "Add the rule")
+            )),
+            tabPanel("Coverage",
+              shiny::plotOutput("ruleplot"),
+              shiny::htmlOutput("variablesCovered"),
+              shiny::htmlOutput("variablesNotCovered")
+            ),            
+            tabPanel("Imposed limits",
+              tags$h3("Boundaries"),
+              tags$em("Limits imposed by the rule set"),
+              shiny::dataTableOutput("num_bdr"),
+              shiny::dataTableOutput("cat_bdr"),
+              tags$h3("Fixed values"),
+              tags$em("Variables which can only take one value under the current rule set"),
+              shiny::dataTableOutput("fixed_variables")
+            )
+          )
+        )
+      )
+    ),
     tabPanel("Confrontation",
-             sidebarLayout(
-               sidebarPanel(
-                 shiny::numericInput("lin.eq.eps"
-                          , label="Tolerance for equality"
-                          , value = 0, min=0, max=Inf),
-                 submitButton("Go!")
-               ),
-               mainPanel(
-                 tabsetPanel(
-                   tabPanel("summary",shiny::dataTableOutput("resultset"))
-                   , tabPanel("plot",shiny::plotOutput("confrontationplot"),
-                              # Download Report Button
-                              downloadButton("report", "Download Report"))
-                   , tabPanel("details", shiny::dataTableOutput("detailset"), 
-                              # Download details Button
-                              downloadButton("detailed_results", "Download Detailed Results"))
-                              )
-                 )
-               )
-             ),
+      sidebarLayout(
+        sidebarPanel(
+          shiny::numericInput(
+            inputId = "lin.eq.eps",
+            label = "Tolerance for equality",
+            value = 0,
+            min = 0,
+            max = Inf
+          ),
+          #submitButton("Go!")
+          actionButton("btn_confront", "Go!")
+        ),
+        mainPanel(
+          tabsetPanel(
+            tabPanel("summary", 
+              shiny::dataTableOutput("resultset")
+            ),
+            tabPanel("plot",
+              shiny::plotOutput("confrontationplot"),
+              # Download Report Button
+              downloadButton("report", "Download Report")
+            ),
+            tabPanel("details",
+              shiny::dataTableOutput("detailset"),
+              # Download details Button
+              downloadButton("detailed_results", "Download Detailed Results")
+            )
+          )
+        )
+      )
+    ),
     tabPanel("Error localization",
-             # sidebarLayout(
-               # sidebarPanel(
-                 # selectInput("rules", "Select rules for inspection",
-                 #             choices = "Confront first",
-                 #             multiple = TRUE)
-                 
-               # ),
-               # mainPanel(
-                 tabsetPanel(
-                   tabPanel("Summary", 
-                            fluidRow(
-                              column(width = 6,
-                              tags$h2("Summary by variable"),
-                              dataTableOutput("el_var")),
-                              # hr(),
-                              column(width = 6,
-                                     tags$h2("Summary by record"),
-                              dataTableOutput("el_rec")))),
-                   tabPanel("Detailed",
-                            
-                              dataTableOutput("el_rows")#,
-                              
-                   )
-                   # )
-                 )
-    ))
+      tabsetPanel(
+        tabPanel("Summary", 
+          fluidRow(
+            column(width = 6,
+              tags$h2("Summary by variable"),
+              shiny::dataTableOutput("el_var")
+            ),
+            column(width = 6,
+              tags$h2("Summary by record"),
+              shiny::dataTableOutput("el_rec")
+            )
+          )
+        ),
+        tabPanel("Detailed",
+          shiny::dataTableOutput("el_rows")
+        )
+      )
+    )
+  )
 )
